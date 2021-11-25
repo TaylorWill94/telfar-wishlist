22747fc919a88870391c0bc1e86645b5e9e469a6
Taylor W <taylorwilliams@pursuit.org>
2021-11-25 15:15:48 -0500

    Telfar Wishlist App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import bags from './data/bags';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      telfars: bags,
+      cart: [],
+
+      subtotal: 0,
+      tax: 0,
+      total: 0,
+    }
+  }
+
+
+  handleTelfar = (event, name, price) => {
+    event.preventDefault();
+    this.setState ({
+      cart: [...this.state.cart, {"name": name, "price": price}],
+    })
+  }
+
+
+
+
+  render() {
+
+    let allTelfars = this.state.telfars.map(telfar => {
+      let { name, price, img } = telfar;
+      return (
+        <div className="products" >
+                    <form>
+                        <img src={img} alt={name} />
+                        <br />
+                        <br />
+                        <h3 >{name}</h3>
+                        <div>Price: {price}</div>
+                        <br />
+                        <button type="submit" onClick={(event)=>this.handleTelfar(event, name, price)}>Telfar Me</button>
+                        <br />
+                        <br />
+                    </form>
+                </div>
+      )
+    })
+
+    let selectedTelfar = this.state.cart.map((car, i) => {
+      return <li key={car.name+i}>
+        
+       <span>Product</span>: {car.name} <br></br>
+       <span>Price</span>: ${car.price}</li>
+    })
+
+
+    return (
+      <div className="App">
+       <p>This is a Telfar Wish List App</p>
+       <div>{ allTelfars }</div>
+       <div className='cart'>
+       <h2>Wishlist:</h2>
+       <ul className='cart-items'> {selectedTelfar} </ul>
+       </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/data/bags.js
@@ -0,0 +1,30 @@
+const bags = [
+    {
+        name: 'Medium Tan Shopping Bag',
+        img: 'https://cdn.shopify.com/s/files/1/0880/7204/products/TELFAR-MEDIUM-TAN-SHOPPING-BAG-FRONT_grande.jpg?v=1633099610',
+        price: 250,
+    },
+    {
+        name: 'Large Black Shopping Bag',
+        img: 'https://cdn.shopify.com/s/files/1/0880/7204/products/TELFAR-LARGE-BLACK-SHOPPING-BAG-FRONT_grande.jpg?v=1633097272',
+        price: 275,
+
+    },
+    {
+        name: 'Small Yellow Shopping Bag',
+        img: 'https://cdn.shopify.com/s/files/1/0880/7204/products/TELFAR-SMALL-YELLOW-SHOPPING-BAG-FRONT_grande.jpg?v=1633101110',
+        price: 150,
+    },
+    {
+        name: 'Small White Shopping Bag',
+        img: 'https://cdn.shopify.com/s/files/1/0880/7204/products/TELFAR-SMALL-YELLOW-SHOPPING-BAG-FRONT_grande.jpg?v=1633101110',
+        price: 150,
+    },
+    {
+        name: 'Small Red Shopping Bag',
+        img: 'https://cdn.shopify.com/s/files/1/0880/7204/products/TELFAR-SMALL-RED-SHOPPING-BAG-FRONT_grande.jpg?v=1633100993',
+        price: 150,
+    },
+]
+
+export default bags
